0810a45a0a4c1e9780406e533da297651e85cefa
Belbel334 <gheyselsdaan@gmail.com>
2024-10-11 22:15:41 +0200

    Added a button to boost income + error message when you don't have enough

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.firstapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,11 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     int Points = 0;
+    int pointperclick = 1;
     Button Mybutton;
+    Button UpgradeButton;
     TextView Counter;
+    TextView Errormessage;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -33,14 +39,38 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         Mybutton =  (Button) findViewById(R.id.button);
+        UpgradeButton = (Button) findViewById(R.id.buybutton);
         Counter = (TextView)findViewById(R.id.textView2);
+        Errormessage = (TextView) findViewById(R.id.Error);
         //Counter.setText("me");
 
         Mybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Points += 1;
-                Counter.setText(String.valueOf(Points));
+                Points += pointperclick;
+                Counter.setText(String.valueOf(Points) + " pontis");
+            }
+        });
+        UpgradeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Points >= 25) {
+                    Points -= 25;
+                    pointperclick += 1;
+                    Counter.setText(String.valueOf(Points) + " pontis");
+                    UpgradeButton.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    Errormessage.setVisibility(View.VISIBLE);
+                    Errormessage.setText("Error: " + "Not enough pontis");
+
+                    new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Errormessage.setVisibility(View.INVISIBLE);
+                        }
+                    }, 1000);
+                }
             }
         });
     }
